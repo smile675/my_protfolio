197787eb399b3c593260bd7e1437c6592f535af9
smile675 <angred.ismail@gmail.com>
2024-04-11 22:10:55 +0800

    added programming skill

---
+++ data/skill.js
@@ -0,0 +1,37 @@
+const programmingSkills = [
+    {
+        logo: "css.png",
+        name: "css"         
+    },
+    {
+        logo: "flutter.png",
+        name: "Flutter"         
+    },
+    {
+        logo: "html.png",
+        name: "HTML"         
+    },
+    {
+        logo: "javascript.png",
+        name: "JavaScript"         
+    },
+    {
+        logo: "php.png",
+        name: "PHP"         
+    },
+    {
+        logo: "python.png",
+        name: "Python"         
+    },
+    {
+        logo: "react.png",
+        name: "React"         
+    },
+    {
+        logo: "sql.png",
+        name: "Database"         
+    }
+
+
+];
+
--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+onLoad()
+
+
+function onLoad(){
+   displayProgrammingSkills();
+}
+
+
+function displayProgrammingSkills(){
+    let skillElements= document.querySelector('.skills_container');
+let innerHtml = '';
+programmingSkills.forEach(skill=>{
+    innerHtml += `
+    <div class="card">
+                    <img class="logo" src="./assets/skill-logo/${skill.logo}"></img>
+                    <div class="title">${skill.name}</div>
+                </div>
+    `;
+});
+
+skillElements.innerHTML = innerHtml;
+
+}
