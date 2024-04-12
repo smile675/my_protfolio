dcc0a2924ca528bee1da4e2faa286a0cb45654ce
smile675 <angred.ismail@gmail.com>
2024-04-12 23:06:53 +0800

    skill and certification completed

--- data/skill.js
+++ data/skill.js
@@ -1,37 +1,109 @@
 const programmingSkills = [
     {
-        logo: "css.png",
-        name: "css"         
-    },
-    {
         logo: "flutter.png",
         name: "Flutter"         
-    },
-    {
-        logo: "html.png",
-        name: "HTML"         
-    },
-    {
-        logo: "javascript.png",
-        name: "JavaScript"         
     },
     {
         logo: "php.png",
         name: "PHP"         
     },
     {
-        logo: "python.png",
-        name: "Python"         
+        logo: "sql.png",
+        name: "Database"         
     },
+    {
+        logo: "html.png",
+        name: "HTML"         
+    },
+    {
+        logo: "css.png",
+        name: "CSS"         
+    },
+    
+    
+    {
+        logo: "javascript.png",
+        name: "JavaScript"         
+    },
+    
+    
     {
         logo: "react.png",
         name: "React"         
     },
     {
-        logo: "sql.png",
-        name: "Database"         
+        logo: "python.png",
+        name: "Python"         
     }
+];
+
+const developmentSkills = [
+    {
+        name: "Mobile App development",
+        level: 90,
+    },
+    {
+        name: "Web development",
+        level: 90,
+    },
+    {
+        name: "Dynamic web application",
+        level: 80,
+    },
+    {
+        name: "Backend development",
+        level: 85,
+    },
+
+    {
+        name: "Database management",
+        level: 70,
+    },
+
+    {
+        name: "Cyber Security",
+        level: 10,
+    },
+
+    
+]
 
 
+const certificationList = [
+    {
+        name: "Bachelor of Mechanical Engineering",
+        complete: true,
+        progress: 100, 
+         
+    },
+    {
+        name: "Flutter & Dart - The Complete Guide at Udemy",
+        complete: true, 
+        progress: 100, 
+
+    },
+    {
+        name: "The Ultimate 2024 Fullstack Web Development Bootcamp at Udemy",
+        complete: true,
+        progress: 100, 
+
+    },
+    {
+        name: "Java 17 Masterclass: Start Coding at Udemy",
+        complete: false,
+        progress: 80, 
+    },
+    {
+        name: "Flutter & Firebase Tutorial: Build 5 Social Media Apps",
+        complete: true,
+        progress: 100, 
+
+    },
+    {
+        name: "C Programming Step by Step - Complete Tutorial For Beginners",
+        complete: false,
+        progress: 10, 
+
+    },
 ];
 
--- index.js
+++ index.js
@@ -4,12 +4,16 @@ onLoad()
 
 function onLoad(){
    displayProgrammingSkills();
+   displayDevelopmentSkills();
+   displayCertifications();
 }
 
 
 function displayProgrammingSkills(){
     let skillElements= document.querySelector('.skills_container');
 let innerHtml = '';
+
+
 programmingSkills.forEach(skill=>{
     innerHtml += `
     <div class="card">
@@ -20,5 +24,63 @@ programmingSkills.forEach(skill=>{
 });
 
 skillElements.innerHTML = innerHtml;
+
+}
+
+function displayDevelopmentSkills(){
+    let developmentSkillElement = document.querySelector('.development_skill');
+    let innerHTML = '';
+
+
+    developmentSkills.forEach(skill=>{
+        let name = skill.name;
+        let level = skill.level;
+        innerHTML += `
+        <div class="skill_card">
+                    <div class="skill_title">${name}</div>
+                    <div class="skill_rating">
+                        <div class="progress" style="width: ${level}%"></div>
+                    </div>
+                </div>
+        `
+    });
+
+    developmentSkillElement.innerHTML = innerHTML;
+
+}
+
+
+function displayCertifications(){
+    let certificateElement = document.querySelector('.certificate_container');
+    let innerHTML = '';
+
+    certificationList.forEach(item => {
+      let name = item.name;
+      let complete = item.complete;
+      let level = item.progress;
+      let icon = complete
+      ? `<i class="fas fa-check-circle" style="color: var(--primary_color)"></i>`
+      :`<i class="far fa-circle"></i>`;
+
+      innerHTML +=`
+      <div class="single_certificate_container">
+
+      <div class="single_certificate"> 
+                    ${icon}
+                    <div class="certificate_title">
+                        ${name}
+                    </div>
+                </div>
+                <div class="skill_rating">
+                <div class="progress" style="width: ${level}%"></div>
+            </div>
+      
+      
+      </div>
+      
+      `
+    });
+
+    certificateElement.innerHTML = innerHTML;
 
 }
